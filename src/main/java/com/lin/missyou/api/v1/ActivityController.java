aa0c0fab7ef4c362a7ff071e197ea55d29771899
wuwenwen <wenwenwenwu@foxmail.com>
2020-05-22 10:56:30 +0800

    斯文

--- src/main/java/com/lin/missyou/api/v1/ActivityController.java
+++ src/main/java/com/lin/missyou/api/v1/ActivityController.java
@@ -27,6 +27,7 @@ public class ActivityController {
 
     @GetMapping("/name/{name}") //返回精简的activity
     public ActivityPureVO getHomeActivity(@PathVariable String name) {
+        System.out.println("miao");
         Activity activity = activityService.getByName(name);
         if (activity == null) {
             throw new NotFoundException(40001);
